adfb5f5569dfd8c220e8c9da838d210a5a9d5ab8
Rohan849-12 <129308407+Rohan849-12@users.noreply.github.com>
2025-03-27 18:52:43 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MyToken", "bcs") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1M tokens to the owner
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
 
